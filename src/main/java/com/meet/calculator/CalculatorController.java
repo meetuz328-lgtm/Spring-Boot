f94f08f1387416428407666c9a1c6dcc0134d757
meetuz328-lgtm <meetuz328@gmail.com>
2026-06-07 19:06:18 +0530

    Changed from Request Parameters to Path variables

--- src/main/java/com/meet/calculator/CalculatorController.java
+++ src/main/java/com/meet/calculator/CalculatorController.java
@@ -1,17 +1,18 @@
 package com.meet.calculator;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalculatorController {
 
-    @GetMapping("/calculate")
+    @GetMapping("calculate/{num1}/{num2}/{operation}")
     public String calculate(
-            @RequestParam double num1,
-            @RequestParam double num2,
-            @RequestParam String operation) {
+            @PathVariable double num1,
+            @PathVariable double num2,
+            @PathVariable String operation) {
 
         double result;
 
